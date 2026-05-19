48c71ea172c47005a294e6a6cefa80abd0249973
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-05-19 15:05:28 -0400

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        freq_s = {}
+        freq_t = {}
+        for char in s:
+            freq_s[char] = 1 + freq_s.get(char, 0)
+        for char in t:
+            freq_t[char] = 1 + freq_t.get(char, 0)
+        return list(freq_s.values()) == list(freq_t.values())
