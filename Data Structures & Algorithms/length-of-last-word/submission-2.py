82875f43f6912108f9dc1ad1171fc3f5365954e9
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-05-19 12:20:31 -0400

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s_len = len(s)
+        res = 0
+        # for i in range(s_len - 1, -1, -1):
+        i = s_len - 1
+        while s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            res += 1
+            i -= 1
+        return res
+        # return len(s.split()[-1])
