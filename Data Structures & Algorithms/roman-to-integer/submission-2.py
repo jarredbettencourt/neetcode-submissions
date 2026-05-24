60d54d68648ac4943dd05693669deb14bd0ea58a
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-05-24 18:18:25 -0400

    Add: roman-to-integer - submission-2

---
+++ Data Structures & Algorithms/roman-to-integer/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        value_mapping = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        stack = []
+        for num in s:
+            num_mapping = value_mapping[num]
+            if stack:
+                if stack[-1] < num_mapping:
+                    val = stack.pop()
+                    stack.append(num_mapping - val)
+                else:
+                    stack.append(num_mapping)
+            else:
+                stack.append(num_mapping)
+        return sum(stack)
