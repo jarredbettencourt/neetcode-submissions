10d43fe5c54ce2acc9e8cfb93c9e770f48cedeca
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-05-24 17:00:50 -0400

    Add: excel-sheet-column-title - submission-4

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = []
+        while columnNumber > 0:
+            rem = columnNumber % 26
+            print(chr(rem + 64))
+            res.append(chr(rem + 64))
+            columnNumber //= 26
+        return "".join(res[::-1])
