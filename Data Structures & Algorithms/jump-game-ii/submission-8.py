3d10b97c6d21738a8cbf7fec84e73a805379abad
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-05-30 22:40:45 -0400

    Add: jump-game-ii - submission-8

---
+++ Data Structures & Algorithms/jump-game-ii/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp = [float('inf')] * len(nums)
+        dp[-1] = 0
+        res = 0
+        for i in range(len(nums) - 2, -1, -1):
+            jumps = nums[i]
+            dp[i] = min(dp[i], 1 + min(dp[i: i + jumps + 1]))
+        return dp[0]
