30e8c0c037896a427359d2b0c9228238392622d5
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-06-02 20:14:31 -0400

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        res = 0
+        arr_len = len(gas)
+
+        def simulate(i, gas, cost):
+            arr_len = len(gas)
+            cur_gas = gas[i]
+            start_idx = i
+            while cur_gas > cost[i]:
+                cur_gas -= cost[i]
+                i = (i + 1) % arr_len
+                if i == start_idx:
+                    return True
+                cur_gas += gas[i]
+            return False
+
+        for i in range(len(gas)):
+            if simulate(i, gas, cost): return i
+        return -1
+                
+        
