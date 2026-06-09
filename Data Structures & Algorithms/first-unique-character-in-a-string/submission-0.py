c32daec2f4bc0536c56517164b2ca2d0c2462516
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-06-09 07:33:58 -0400

    Add: first-unique-character-in-a-string - submission-0

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        string_map = {}
+        for idx, char in enumerate(s):
+            if char not in string_map:
+                string_map[char] = idx
+            else:
+                del string_map[char]
+        if string_map:
+            return min(string_map.values())
+        return -1
