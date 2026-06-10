c58d4229c814951fbc27e7df1e479f0727958817
Jarred Bettencourt <42685268+jarredbettencourt@users.noreply.github.com>
2026-06-09 20:25:08 -0400

    Add: arranging-coins - submission-0

---
+++ Data Structures & Algorithms/arranging-coins/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        i = 1
+        while True:
+            n -= i
+            if n <= 0:
+                return i - 1
+            i += 1
